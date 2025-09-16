0f71055f381e01a1a59d02dad9660cd810520c4b
spectresingularity17 <spectresingularity17@gmail.com>
2025-09-16 16:50:51 -0600

    cosmetic upgrades/enter key
    now the enter key does stuff, and things are green

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,19 +1,26 @@
-import {useState} from "react";
+import {useState} from "react"
 
 export function TodoInput(props) {
-    const { handleAddTodo } = props;
-    const [inputValue, setInputValue] = useState("");
+    const { handleAddTodo } = props
+    const [inputValue, setInputValue] = useState("")
+
+    function checkAdd(inputValue) {
+        if (!inputValue) {return}
+        handleAddTodo(inputValue)
+        setInputValue('')
+    }
 
     return (
         <div className='input-container'>
             <input value={inputValue} onChange={(e) => {
-                setInputValue(e.target.value);
+                setInputValue(e.target.value)
+                addEventListener("keypress", (e) => {
+                    if (e.key === "Enter") {checkAdd(inputValue)}
+                })
             }} placeholder='Add task'/>
             <button onClick={() => {
-                if (!inputValue) {return}
-                handleAddTodo(inputValue)
-                setInputValue('')}
-            }>
+                checkAdd(inputValue)
+            }}>
                 <i className='fa-solid fa-plus'></i>
             </button>
         </div>
